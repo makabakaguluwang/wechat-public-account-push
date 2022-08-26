c76dcb9aad0331cf8d995c89b76032e37dcdfb93
makabakaguluwang <112187453+makabakaguluwang@users.noreply.github.com>
2022-08-26 23:21:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,22 +1,22 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx2c1a3070ca20bcc8",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "a5753f985fa801c3ba71a2c3fc913b6d",
     // 模板消息id
-    templateId: "",
+    templateId: "sIoEr0Q8hBvs2uiQPqmE7S6GVRxf2FIobYkYQwDM808",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [oHvrW5qkbkS7EknLOGRdWwfJuDsQ],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "江苏",
     // 所在城市
-    city: "惠州",
+    city: "南京",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
+      {"name": "小馨", "year": "2000", "date": "11-11", "type": 'new'},
       {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
     ],
     // 在一起的日子，格式同上
